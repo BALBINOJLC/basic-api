62f76c07899c8fadefc152c4cf4b76d05e5875a1
Miguel Sanz <msanz1610@gmail.com>
2023-03-21 14:52:09 -0300

    feat: se eliminan los modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppImports } from './app.import';
 import { CaslModule } from './casl/casl.module';
 import { HealthModule } from './health/health.module';
-import { AuthModule, UsersModule } from './v1';
 
 @Module({
-    imports: [...AppImports, HealthModule, CaslModule, AuthModule, UsersModule],
+    imports: [...AppImports, HealthModule, CaslModule],
     controllers: [],
     providers: [],
 })
