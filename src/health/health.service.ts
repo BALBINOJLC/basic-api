df3dcfafa60d10d4392e3df671982c184068f3c2
Miguel Sanz <msanz1610@gmail.com>
2024-01-03 16:46:30 -0300

    update

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -6,7 +6,10 @@ import { HealthDto } from './health.dto';
 
 @Injectable()
 export class HealthService {
-    constructor(private configService: ConfigService, private logger: Logger) {}
+    constructor(
+        private configService: ConfigService,
+        private logger: Logger
+    ) {}
 
     getHealthCheck(): HealthDto {
         this.logger.log('getHealthCheck in version %o', `${HealthService.name}:${this.getHealthCheck.name}`, process.version);
@@ -25,6 +28,7 @@ export class HealthService {
         return 'Ok!';
     }
 
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     getWhoami(user: any): string {
         this.logger.log({ user }, `${HealthService.name}:${this.getWhoami.name}`);
         return `Hi ${user.userName}`;
