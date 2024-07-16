1639d82780b25c6f4f2984752a3fc772c1e570ad
Miguel Sanz <msanz1610@gmail.com>
2024-07-16 13:21:34 -0400

    chore(auth): update authentication DTOs and remove unused schemas

--- src/v1/auth/dtos/auth.dto.ts
+++ src/v1/auth/dtos/auth.dto.ts
@@ -1,5 +1,27 @@
-import { EUserRole } from '@prisma/client';
 import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, IsStrongPassword } from 'class-validator';
+
+export class LoginUserDto {
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @IsStrongPassword()
+    password: string;
+}
+
+export class PasswordForgotDto {
+    @IsEmail()
+    email: string;
+}
+
+export class ResetPasswordDto {
+    @IsString()
+    @IsStrongPassword()
+    password: string;
+}
+
+import { EUserRole } from '@prisma/client';
 
 export class RegisterUserDto {
     @IsString()
