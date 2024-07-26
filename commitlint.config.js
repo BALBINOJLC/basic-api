b57948956a8c81afd2819f74960e9a58ecaef8e9
Miguel Sanz <msanz1610@gmail.com>
2024-07-26 09:02:46 -0400

    chore(commitlint): increase header max length to 1000 characters

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,6 +9,6 @@ module.exports = {
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 100]
+    'header-max-length': [2, 'always', 1000]
   }
 };
