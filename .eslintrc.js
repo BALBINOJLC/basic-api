740a873d0d312a3a635d7284c67dfb200454b308
Miguel Sanz <msanz1610@gmail.com>
2024-10-11 08:13:29 -0300

    fix(refactor): ajustes varios

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,37 +16,97 @@ module.exports = {
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        "selector": "interface",
-        "format": ["PascalCase"],
-        "custom": {
-          "regex": "^I[A-Z]",
-          "match": true
-        }
-      },
-      {
-        "selector": "typeAlias",
-        "format": ["PascalCase"],
-        "custom": {
-          "regex": "^T[A-Z]",
-          "match": true
-        }
-      },
-      {
-        "selector": "enum",
-        "format": ["PascalCase"],
-        "custom": {
-          "regex": "^E[A-Z]",
-          "match": true
-        }
+  "@typescript-eslint/naming-convention": [
+    "error",
+    {
+      "selector": "interface",
+      "format": ["PascalCase"],
+      "custom": {
+        "regex": "^I[A-Z]",
+        "match": true
       }
-    ]
-  },
+    },
+    {
+      "selector": "class",
+      "format": ["PascalCase"]
+    },
+    {
+      "selector": "typeParameter",
+      "format": ["PascalCase"],
+      "custom": {
+        "regex": "^T[A-Z]",
+        "match": true
+      }
+    },
+    {
+      "selector": "variable",
+      "format": ["camelCase", "UPPER_CASE"],
+      "leadingUnderscore": "allow"
+    },
+    {
+      "selector": "function",
+      "format": ["camelCase"]
+    },
+    {
+      "selector": "parameter",
+      "format": ["camelCase"],
+      "leadingUnderscore": "allow"
+    },
+    {
+      "selector": "property",
+      "format": ["camelCase", "PascalCase", "snake_case"]
+    },
+    {
+      "selector": "method",
+      "format": ["camelCase"]
+    },
+    {
+      "selector": "enumMember",
+      "format": ["PascalCase"]
+    },
+    {
+      "selector": "property",
+      "modifiers": ["static"],
+      "format": ["camelCase", "UPPER_CASE"]
+    },
+    {
+      "selector": "memberLike",
+      "modifiers": ["private"],
+      "format": ["camelCase"],
+      "leadingUnderscore": "require"
+    },
+    {
+      "selector": "memberLike",
+      "modifiers": ["protected"],
+      "format": ["camelCase"],
+      "leadingUnderscore": "require"
+    },
+    {
+      "selector": "memberLike",
+      "modifiers": ["public"],
+      "format": ["camelCase"]
+    },
+    {
+      "selector": "memberLike",
+      "modifiers": ["protected"],
+      "format": ["camelCase"],
+      "leadingUnderscore": "require"
+    },
+    {
+      "selector": "typeAlias",
+      "format": ["PascalCase"],
+      "custom": {
+        "regex": "^T[A-Z]",
+        "match": true
+      }
+    },
+    {
+      "selector": "enum",
+      "format": ["PascalCase"],
+      "custom": {
+        "regex": "^E[A-Z]",
+        "match": true
+      }
+    }
+  ]
 };
